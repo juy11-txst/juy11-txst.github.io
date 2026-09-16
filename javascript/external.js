3745b52a8998e97d2be3fa33fab015082569273c
Christopher Quiroga <juy11@txstate.edu>
2026-09-16 13:44:54 -0500

    saved n

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,3 @@
 user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
 var headerElement = document.querySelector("#header h1");
 headerElement.textContent = "Hello, welcome " + user_name;
