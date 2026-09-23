adccfd4fcaae0ff521fbf5f73965840ac61f6d94
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 12:45:28 -0500

    5

--- javascript/external.js
+++ javascript/external.js
@@ -9,21 +9,6 @@ let b = "Type your name here.";
 user_name = window.prompt(a, b);
 message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
 return message
-var webmaps =
-[
-  ["Google Maps", "https://www.google.com/maps", "The primary focus of this
-consumer-focused web mapping application is to help users navigate and explore
-their physical surroundings. It offers geocoding and proximity analysis in terms
-of GIS functionality within the app. It was designed for general use, being
-widely used by many people in the world."],
-  ["USGS National Map Viewer", "https://apps.nationalmap.gov/viewer/", "The
-primary focus of this government operated GIS web application is to provide the
-public with access to geospatial data and topographic information for the
-United States. It offers a variety of GIS tools such as 3D elevation profile
-analysis, buffer tools, and external data integration. It was designed for
-researchers, land managers, and scientists but it is widely used by the public
-as well, such as hikers or surveyors."]
-];
 }
 function webmap_table()
 {
