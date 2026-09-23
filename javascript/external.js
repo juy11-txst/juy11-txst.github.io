274e954e9c558346ece2c2941762a15904ac99cb
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 13:24:22 -0500

    12

--- javascript/external.js
+++ javascript/external.js
@@ -1,13 +1,24 @@
-var webmaps =
-[
-["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit
-developed by Enterprise Technology Solutions is neat."],
-["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The
-Texas Parks and Wildlife's Landscape Ecology program is great."]
-];
 // user_name = window.prompt("Please enter your name", "Type your name here");
 // var headerElement = document.querySelector("#header h1");
 // headerElement.textContent = "Hello, welcome " + user_name;
+// Webmap Data Arrays
+const wm_names = [
+    "Oil Spill Toolkit",
+    "Texas Ecosystems Analytical Mapper"
+];
+
+const wm_links = [
+    "https://texas.gov",
+    "http://texas.gov"
+];
+const wm_paragraphs = [
+    "The oil spill toolkit developed by Enterprise Technology Solutions is neat.",
+    "The Texas Parks and Wildlife's Landscape Ecology program is great."
+];
+const webmaps = [
+    [wm_names[0], wm_links[0], wm_paragraphs[0]],
+    [wm_names[1], wm_links[1], wm_paragraphs[1]]
+];
 function welcome()
 {
 let a = "Please enter your name.";
