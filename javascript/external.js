8f29ac2d56d6033f32ee8dcb81cf7898b3c741dc
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 17:49:44 -0500

    3

--- javascript/external.js
+++ javascript/external.js
@@ -43,7 +43,6 @@ function webmap_table()
   document.write("<table width=100%>");
 for (var row=0; row < webmaps.length; row++) {
 if (row % 2 === 0) {
-  } else {
   }
 document.write("<tr>");
 for (var column=0; column < webmaps[row].length; column++)
