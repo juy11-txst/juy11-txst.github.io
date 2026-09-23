17d5d857090b8ce1a97ae0df4ba6f24014af28d4
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 12:51:07 -0500

    5

--- javascript/external.js
+++ javascript/external.js
@@ -12,15 +12,15 @@ return message
 }
 function webmap_table()
 {
-document.write("<table width=100%>");
-for (var row=0; row < webmaps.length; row++)
+  document.write("<table width=100%>");
+for (var row=0; row < 2; row++)
 {
- document.write("<tr>");
- for (var column=0; column < webmaps[0].length; column++)
+document.write("<tr>");
+for (var column=0; column < 3; column++)
 {
-    document.write("<td>" + webmaps[row][column] + "</td>");
-  }
-  document.write("</tr>");
+document.write("<td>" + row + "," + column + "</td>");
+}
+document.write("</tr>");
 }
 document.write("</table>");
 return "";
