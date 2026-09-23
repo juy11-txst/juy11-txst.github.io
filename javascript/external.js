64fc5dbb3427f333ba42380f2f78e2ed0ed42385
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 10:23:07 -0500

    3

--- javascript/external.js
+++ javascript/external.js
@@ -7,7 +7,7 @@ let a = "Please enter your name.";
 let b = "Type your name here.";
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt(a, b);
-message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+let message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
 return message
 }
 document.write(welcome());
