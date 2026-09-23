fa5ba0095198740f6d28ad49dd3f1221d8969d8b
Christopher Quiroga <juy11@txstate.edu>
2026-09-23 13:46:11 -0500

    2

--- javascript/external.js
+++ javascript/external.js
@@ -12,11 +12,12 @@ const wm_links = [
     "https://apps.nationalmap.gov/viewer/"
 ];
 const wm_paragraphs = [
-    "The primary focus of this consumer-focused web mapping application is to
-    help users navigate and explore their physical surroundings.",
+  "The primary focus of this consumer-focused web mapping application is to help
+   users navigate and explore their physical surroundings.",
     "The primary focus of this government operated GIS web application is to
-    provide the public with access to geospatial data and topographic
-     information."
+     provide the public with access to geospatial data and topographic
+      information."
+];
 
 ];
 const webmaps = [
